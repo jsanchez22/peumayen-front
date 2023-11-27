b8261f79113b217346386e8999320aae7c97f90e
Jeanfranco Sanchez (CTGB) <jeanfranco.sanchez@AES.COM>
2023-11-27 14:09:31 -0300

    terminado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,13 @@ export const Footer = () => {
     <nav>
       <div className="footer">
   <div className="redes-sociales">
-    <a href="enlace-a-tu-facebook" target="_blank" rel="noopener noreferrer">
+    <a href="enlace-facebook" target="_blank" rel="noopener noreferrer">
       <img src="./images/facebook.png" alt="Facebook" className="redes" />
     </a>
-    <a href="enlace-a-tu-twitter" target="_blank" rel="noopener noreferrer">
+    <a href="enlace-twitter" target="_blank" rel="noopener noreferrer">
       <img src="./images/x.png" alt="Twitter" className="redes"/>
     </a>
-    <a href="enlace-a-tu-instagram" target="_blank" rel="noopener noreferrer">
+    <a href="enlace-instagram" target="_blank" rel="noopener noreferrer">
       <img src="./images/instagram.png" alt="Instagram" className="redes"/>
     </a>
   </div>
